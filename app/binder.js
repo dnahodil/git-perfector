f8194b94371db8f2e63c9cbd5bcf7cf5d1636a8f
David Nahodil <dnahodil@gmail.com>
2020-04-30 15:30:52 +1000

    Project is out of date. Will be started from scratch.

--- app/binder.js
+++
@@ -1,9 +0,0 @@
-$ = exports.$ = (elementId) => document.getElementById(elementId)
-
-exports.bindAction = function(elementId, fn) {
-  $(elementId).addEventListener("click", fn)
-}
-
-exports.bindOutput = function(elementId, data) {
-  $(elementId).innerHTML = data
-}
